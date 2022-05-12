09afa34de4da2333abc6671c5f37eacbca283e9c
YuyaHiguchi <k021g1263@g.neec.ac.jp>
2022-05-12 14:22:50 +0900

    3の課題のところまで

--- main.cpp
+++ main.cpp
@@ -254,7 +254,20 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
         {-0.5f,-0.5f,0.0f},//左下
         {-0.5f,+0.5f,0.0f},//左上
         {+0.5f,-0.5f,0.0f},//右下
+        {+0.5f,+0.5f,0.0f}//右上
+        //{+0.5f,0.0f,0.0f},//右下
+        //{-0.5f,+0.75f,0.0f},//左上
+        
     };
+
+    //インデックスデータ
+    uint16_t indices[] =
+    {
+        0,1,2,//三角形一つ目
+        1,2,3,//三角形二つ目
+    };
+    //インデックスデータ全体のサイズ
+    UINT sizeIB = static_cast<UINT>(sizeof(uint16_t) * _countof(indices));
 
     //頂点データ全体のサイズ＝頂点データ一つ分のサイズ＊頂点データの要素数
     UINT sizeVB = static_cast<UINT>(sizeof(XMFLOAT3) * _countof(vertices));
@@ -262,6 +275,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
     //頂点バッファの設定
     D3D12_HEAP_PROPERTIES heapProp{};  //ヒープ設定
     heapProp.Type = D3D12_HEAP_TYPE_UPLOAD;  //GPUへの転送用
+
     //リソース設定
     D3D12_RESOURCE_DESC resDesc{};
     resDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
@@ -271,6 +285,13 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
     resDesc.MipLevels = 1;
     resDesc.SampleDesc.Count = 1;
     resDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+
+
+
+
+
+   
+
 
     //頂点バッファの生成
     ID3D12Resource* vertBuff = nullptr;
@@ -370,6 +391,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
     // グラフィックスパイプライン設定
     D3D12_GRAPHICS_PIPELINE_STATE_DESC pipelineDesc{};
 
+
+
     // シェーダーの設定
     pipelineDesc.VS.pShaderBytecode = vsBlob->GetBufferPointer();
     pipelineDesc.VS.BytecodeLength = vsBlob->GetBufferSize();
@@ -385,8 +408,35 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
     pipelineDesc.RasterizerState.DepthClipEnable = true; // 深度クリッピングを有効に
 
     // ブレンドステート
-    pipelineDesc.BlendState.RenderTarget[0].RenderTargetWriteMask
-        = D3D12_COLOR_WRITE_ENABLE_ALL; // RBGA全てのチャンネルを描画
+    //pipelineDesc.BlendState.RenderTarget[0].RenderTargetWriteMask = D3D12_COLOR_WRITE_ENABLE_ALL; // RBGA全てのチャンネルを描画
+   
+    //レンダーターゲットのブレンド設定
+    D3D12_RENDER_TARGET_BLEND_DESC& blenddesc = pipelineDesc.BlendState.RenderTarget[0];
+    blenddesc.RenderTargetWriteMask = D3D12_COLOR_WRITE_ENABLE_ALL;
+    blenddesc.BlendEnable = true;
+    blenddesc.BlendOpAlpha = D3D12_BLEND_OP_ADD;
+    blenddesc.SrcBlendAlpha = D3D12_BLEND_ONE;
+    blenddesc.DestBlendAlpha = D3D12_BLEND_ZERO;
+    
+    ////加算合成
+    //blenddesc.BlendOp = D3D12_BLEND_OP_ADD; //加算
+    //blenddesc.SrcBlend = D3D12_BLEND_ONE; //ソースの値を100%使う
+    //blenddesc.DestBlend = D3D12_BLEND_ONE; //デストの値を100%使う
+
+    ////減算合成
+    //blenddesc.BlendOp = D3D12_BLEND_OP_REV_SUBTRACT;
+    //blenddesc.SrcBlend = D3D12_BLEND_ONE;
+    //blenddesc.DestBlend = D3D12_BLEND_ONE;
+
+    ////色反転
+    //blenddesc.BlendOp = D3D12_BLEND_OP_ADD;
+    //blenddesc.SrcBlend = D3D12_BLEND_INV_DEST_COLOR;
+    //blenddesc.DestBlend = D3D12_BLEND_ZERO;
+
+    //半透明合成
+    blenddesc.BlendOp = D3D12_BLEND_OP_ADD;
+    blenddesc.SrcBlend = D3D12_BLEND_SRC_ALPHA;
+    blenddesc.DestBlend = D3D12_BLEND_INV_SRC_ALPHA;
 
     // 頂点レイアウトの設定
     pipelineDesc.InputLayout.pInputElementDescs = inputLayout;
@@ -403,8 +453,19 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
     // ルートシグネチャ
     ID3D12RootSignature* rootSignature;
     // ルートシグネチャの設定
+   
+    //ルートパラメータの設定
+    D3D12_ROOT_PARAMETER rootParam = {};
+    rootParam.ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;
+    rootParam.Descriptor.ShaderRegister = 0;
+    rootParam.Descriptor.RegisterSpace = 0;
+    rootParam.ShaderVisibility = D3D12_SHADER_VISIBILITY_ALL;
+
     D3D12_ROOT_SIGNATURE_DESC rootSignatureDesc{};
     rootSignatureDesc.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
+    rootSignatureDesc.pParameters = &rootParam;
+    rootSignatureDesc.NumParameters = 1;
+
     // ルートシグネチャのシリアライズ
     ID3DBlob* rootSigBlob = nullptr;
     result = D3D12SerializeRootSignature(&rootSignatureDesc, D3D_ROOT_SIGNATURE_VERSION_1_0,
@@ -422,7 +483,84 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
     result = device->CreateGraphicsPipelineState(&pipelineDesc, IID_PPV_ARGS(&pipelineState));
     assert(SUCCEEDED(result));
 
+    //定数バッファ用データ構造体（マテリアル）
+    struct ConstBufferDataMaterial {
+        XMFLOAT4 color;//色（RGBA）
+    };
 
+    //ヒープ設定
+    D3D12_HEAP_PROPERTIES cbHeapProp{};
+    cbHeapProp.Type = D3D12_HEAP_TYPE_UPLOAD;
+
+    //リソース設定
+    D3D12_RESOURCE_DESC cbResourceDesc{};
+    cbResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+    cbResourceDesc.Width = (sizeof(ConstBufferDataMaterial) + 0xff) & ~0xff;
+    cbResourceDesc.Height = 1;
+    cbResourceDesc.DepthOrArraySize = 1;
+    cbResourceDesc.MipLevels = 1;
+    cbResourceDesc.SampleDesc.Count = 1;
+    cbResourceDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+
+    ID3D12Resource* constBuffMaterial = nullptr;
+
+    //定数バッファの生成
+    result = device->CreateCommittedResource(
+        &cbHeapProp,
+        D3D12_HEAP_FLAG_NONE,
+        &cbResourceDesc,
+        D3D12_RESOURCE_STATE_GENERIC_READ,
+        nullptr,
+        IID_PPV_ARGS(&constBuffMaterial));
+    assert(SUCCEEDED(result));
+
+    //定数バッファのマッピング
+    ConstBufferDataMaterial* constMapMaterial = nullptr;
+    result = constBuffMaterial->Map(0, nullptr, (void**)&constMapMaterial);
+    assert(SUCCEEDED(result));
+
+
+
+    //値を書き込むと自動的に転送される
+    constMapMaterial->color = XMFLOAT4(1, 0, 0, 0.5f);
+
+    //リソース設定
+
+    resDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+    resDesc.Width = sizeIB; //頂点データ全体のサイズ
+    resDesc.Height = 1;
+    resDesc.DepthOrArraySize = 1;
+    resDesc.MipLevels = 1;
+    resDesc.SampleDesc.Count = 1;
+    resDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+
+    //インデックスバッファの生成
+    ID3D12Resource* indexBuff = nullptr;
+    result = device->CreateCommittedResource(
+        &heapProp,
+        D3D12_HEAP_FLAG_NONE,
+        &resDesc,
+        D3D12_RESOURCE_STATE_GENERIC_READ,
+        nullptr,
+        IID_PPV_ARGS(&indexBuff)
+    );
+
+    //インデックスバッファをマッピング
+    uint16_t* indexMap = nullptr;
+    result = indexBuff->Map(0, nullptr, (void**)&indexMap);
+    //全インデックスに対して
+    for (int i = 0; i < _countof(indices); i++)
+    {
+        indexMap[i] = indices[i]; //インデックスをコピー
+    }
+    //マッピング解除
+    indexBuff->Unmap(0, nullptr);
+
+    //インデックスバッファビューの作成
+    D3D12_INDEX_BUFFER_VIEW ibView{};
+    ibView.BufferLocation = indexBuff->GetGPUVirtualAddress();
+    ibView.Format = DXGI_FORMAT_R16_UINT;
+    ibView.SizeInBytes = sizeIB;
 
 #pragma endregion
 
@@ -484,7 +622,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 
         // ビューポート設定コマンド
         D3D12_VIEWPORT viewport{};
-        viewport.Width = window_width/2;   //横幅
+        viewport.Width = window_width;   //横幅
         viewport.Height = window_height; //縦幅
         viewport.TopLeftX = 0;           //左上X
         viewport.TopLeftY = 0;           //左上Y
@@ -497,9 +635,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
         // シザー矩形
         D3D12_RECT scissorRect{};
         scissorRect.left = 0;                                 // 切り抜き座標左
-        scissorRect.right = scissorRect.left + window_width/4;  // 切り抜き座標右
-        scissorRect.top = window_height/2;                         // 切り抜き座標上
+        scissorRect.right = scissorRect.left + window_width;  // 切り抜き座標右
+        scissorRect.top = 0;                                  // 切り抜き座標上
         scissorRect.bottom = scissorRect.top + window_height; // 切り抜き座標下
+
         // シザー矩形設定コマンドを、コマンドリストに積む
         commandList->RSSetScissorRects(1, &scissorRect);
 
@@ -513,9 +652,16 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
         // 頂点バッファビューの設定コマンド
         commandList->IASetVertexBuffers(0, 1, &vbView);
 
-        // 描画コマンド
-        commandList->DrawInstanced(_countof(vertices), 1, 0, 0); // 全ての頂点を使って描画
+        //定数バッファビュー（CBV）の設定コマンド
+        commandList->SetGraphicsRootConstantBufferView(0, constBuffMaterial->GetGPUVirtualAddress());
 
+        //インデックスバッファビューの設定コマンド
+        commandList->IASetIndexBuffer(&ibView);
+
+        // 描画コマンド
+        commandList->DrawIndexedInstanced(_countof(indices), 1, 0, 0, 0); // 全ての頂点を使って描画
+
+        
 
 #pragma endregion
         //４．描画コマンドここまで
